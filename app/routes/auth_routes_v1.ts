bca6fef524c9bc4238f3fd1bc1a88b3d46b58c15
Ic Box <dev@icbox.com.br>
2026-01-14 15:55:44 -0300

    Deploy: Ajste rotas

--- app/routes/auth_routes_v1.ts
+++ app/routes/auth_routes_v1.ts
@@ -1,23 +1,20 @@
-import AppKeysController from '#controllers/app_keys_controller'
+// import AppKeysController from '#controllers/app_keys_controller'
 import AppsController from '#controllers/apps_controller'
 import AuthController from '#controllers/auth_controller'
-import UsersController from '#controllers/users_controller'
+// import UsersController from '#controllers/users_controller'
 import { UserRole } from '#services/auth/role_service'
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
 router
   .group(() => {
-    router.post('email/send', [AppsController, 'sendEmail']).middleware(middleware.authRole(UserRole.USER))
+    router
+      .post('email/send', [AppsController, 'sendEmail'])
+      .middleware(middleware.authRole(UserRole.USER))
     router.post('login', [AuthController, 'login'])
-    router.post('password/reset-request', [AuthController, 'requestPasswordReset' ])
+    router.post('password/reset-request', [AuthController, 'requestPasswordReset'])
     router.post('password/reset', [AuthController, 'resetPassword'])
 
-    router.group(() => {
-      router.post('create', [AuthController, 'createSuperUser'])
-      router.get('check', [AuthController, 'checkStartSetup'])
-    }).prefix('setup')
-    
     router
       .group(() => {
         router.post('logout', [AuthController, 'logout'])
@@ -31,12 +28,5 @@ router
       .prefix('auth')
       .middleware(middleware.authRole(UserRole.VIEW))
 
-    router
-      .group(() => {
-        router.resource('users', UsersController).apiOnly().as('admin.users')
-        router.resource('keys', AppKeysController).apiOnly().as('admin.keys')
-      })
-      .prefix('admin')
-      // .middleware(middleware.authRole(UserRole.ADMIN))
   })
   .prefix('api/v1')
--- start/routes.ts
+++ start/routes.ts
@@ -10,8 +10,9 @@
 import router from '@adonisjs/core/services/router'
 import transmit from '@adonisjs/transmit/services/main'
 import { middleware } from './kernel.js'
+import AuthController from '#controllers/auth_controller'
 
-import '#routes/app_routes_v1'
+import '#routes/auth_routes_v1'
 import '#routes/sys_routes_v1'
 import '#routes/app_test'
 import '#routes/webhook_routes_v1'
@@ -28,3 +29,18 @@ transmit.registerRoutes((route) => {
     return
   }
 })
+
+router
+  .group(() => {
+    router.post('create', [AuthController, 'createSuperUser'])
+    router.get('check', [AuthController, 'checkStartSetup'])
+  })
+  .prefix('setup')
+
+// router
+//   .group(() => {
+//     router.resource('users', UsersController).apiOnly().as('admin.users')
+//     router.resource('keys', AppKeysController).apiOnly().as('admin.keys')
+//   })
+//   .prefix('admin')
+//   // .middleware(middleware.authRole(UserRole.ADMIN))
